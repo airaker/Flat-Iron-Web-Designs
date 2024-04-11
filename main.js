4c5fc93400e476eabe1ffa7348e6d3f8856ef3b1
airaker <157429741+airaker@users.noreply.github.com>
2024-04-10 22:17:25 -0600

    update site

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
+
+    var swiper = new Swiper(".mySwiper", {
+      effect: "cards",
+      grabCursor: true,
+    });
+
 
 const scrollers = document.querySelectorAll('.scroller')
 
